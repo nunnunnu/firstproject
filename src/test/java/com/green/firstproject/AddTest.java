76fe59d5a3ce42085e98c2e20fa15482d39b24f9
jinhee <bis04199@gmail.com>
2023-01-03 16:28:35 +0900

    test

--- src/test/java/com/green/firstproject/AddTest.java
+++ src/test/java/com/green/firstproject/AddTest.java
@@ -1,14 +1,19 @@
 package com.green.firstproject;
 
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.green.firstproject.entity.BurgerInfoEntity;
 import com.green.firstproject.entity.CategoryEntity;
+import com.green.firstproject.entity.GradeInfoEntity;
+import com.green.firstproject.entity.MemberInfoEntity;
+import com.green.firstproject.entity.MenuInfoEntity;
+import com.green.firstproject.entity.OrderInfoEntity;
 import com.green.firstproject.repository.BurgerInfoRepository;
 import com.green.firstproject.repository.CategoryRepository;
 
@@ -17,12 +22,23 @@ import com.green.firstproject.repository.CategoryRepository;
 public class AddTest {
      @Autowired BurgerInfoRepository biRepo;
      @Autowired CategoryRepository cRepo;
+     @Autowired OrderInfoEntity oiRepo;
 
      @Test
+     @Transactional
      void 버거정보추가(){
           Optional<CategoryEntity> c = cRepo.findById(1L);
-          BurgerInfoEntity b = new BurgerInfoEntity(null, "치즈버거", c.get(), "치즈버거입니다", "file.jpg", "치즈버거", null, null);
+          BurgerInfoEntity b = new BurgerInfoEntity(null, "치즈버거1", c.get(), "치즈버거입니다", "file.jpg", "치즈버거", null, null);
           System.out.println(b);
           biRepo.save(b);
+     }
+     @Test
+     @Transactional
+     void 카테고리추가(){
+          CategoryEntity c = new CategoryEntity(null, "카테고리1");
+          cRepo.save(c);
+     }
+     @Test
+     void 주문(){
      }
 }
