985dfcefa0d0da8482d12662a649c8fccbb974ae
jinhee <bis04199@gmail.com>
2023-01-03 14:33:29 +0900

    entity+repository

---
+++ src/main/java/com/green/firstproject/entity/SideInfoEntity.java
@@ -0,0 +1,26 @@
+package com.green.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="side_info")
+public class SideInfoEntity {
+     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name="side_seq") private Long sideSeq;
+     @Column(name="side_name") private String sideName;
+     @Column(name="side_cate") private Long sideCate;
+     @Column(name="side_detail") private String sideDetail;
+     @Column(name="side_file") private String sideFile;
+     @Column(name="side_uri") private String sideUri;
+}
