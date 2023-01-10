f42bd65a76d1ce59bfd9a7aa780131c40b7c09c6
inwonjung <inwon123@naver.com>
2023-01-10 14:21:55 +0900

    hia6

--- src/main/java/com/green/firstproject/entity/menu/sellermenu/EventInfoEntity.java
+++ src/main/java/com/green/firstproject/entity/menu/sellermenu/EventInfoEntity.java
@@ -2,9 +2,7 @@ package com.green.firstproject.entity.menu.sellermenu;
 import java.time.LocalDate;
 
 import com.green.firstproject.entity.menu.CategoryEntity;
-=======
 import com.fasterxml.jackson.annotation.JsonIgnore;
->>>>>>> 0d63a797f85ac281b62feab90f4efa0206f8722e
 import com.green.firstproject.entity.menu.basicmenu.DrinkInfoEntity;
 import com.green.firstproject.vo.menu.HiaEventAddVO;
 
@@ -34,19 +32,9 @@ public class EventInfoEntity {
      @Column(name="ei_end_dt") private LocalDate eiEndDt;
      @Column(name="ei_price") private Integer eiPrice;
      @Column(name="ei_detail") private String eiDetail;
-<<<<<<< HEAD
-     @ManyToOne 
-     @JoinColumn(name="ei_di_seq") private DrinkInfoEntity diSeq;
-     @ManyToOne 
-     @JoinColumn(name="ei_di2_seq") private DrinkInfoEntity di2Seq;
-     //  @Column(name="ei_cate") private Long eiCate;
-     @ManyToOne 
-     @JoinColumn(name="ei_cate") private CategoryEntity cate;
-=======
      @ManyToOne(fetch = FetchType.LAZY) @JsonIgnore @JoinColumn(name="ei_di_seq") private DrinkInfoEntity eiDiSeq;
      @ManyToOne(fetch = FetchType.LAZY) @JsonIgnore @JoinColumn(name="ei_di2_seq") private DrinkInfoEntity eiDi2Seq;
-     @Column(name="ei_cate") private Long eiCate;
->>>>>>> 0d63a797f85ac281b62feab90f4efa0206f8722e
+     @ManyToOne(fetch = FetchType.LAZY)  @JoinColumn(name="ei_cate") private CategoryEntity cate;
      @Column(name="ei_file") private String eiFile;
      @Column(name="ei_uri") private String eiUri;
 
