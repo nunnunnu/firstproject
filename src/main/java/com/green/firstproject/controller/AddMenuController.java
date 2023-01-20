6a4bebfbabee5c1cad7b83858b9b4e9058c9107b
eun <eu-nee@naver.com>
2023-01-20 10:45:25 +0900

    23012000

---
+++ src/main/java/com/green/firstproject/controller/AddMenuController.java
@@ -0,0 +1,8 @@
+package com.green.firstproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/menu/add")
+public class AddMenuController {
+    
+}
