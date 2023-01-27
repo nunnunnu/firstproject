38dba9e15d8ad60ae4c96e68bdcbd0ee0046d943
inwonjung <inwon123@naver.com>
2023-01-27 09:36:11 +0900

    inwon

--- src/main/java/com/green/firstproject/service/menu/MenuInfoService.java
+++ src/main/java/com/green/firstproject/service/menu/MenuInfoService.java
@@ -211,7 +211,7 @@ public class MenuInfoService {
     // }
 
     public Map<String, Object> showIngredient(Long menuSeq){
-        Map<String, Object> map = new LinkedHashMap();
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
         MenuInfoEntity menu = menuRepo.findByMenuSeq(menuSeq);
         if(menu==null){
             map.put("status", false);
